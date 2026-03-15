a3fc0e622bbb0f67fbe0f1c27d48a0950bc415b5
WEB5D <info@blkluv.org>
2026-03-15 14:33:31 -0400

    Update web.js

--- web-src/web.js
+++ web-src/web.js
@@ -1,15 +1,17 @@
-import Map from 'ol/Map';
-import Overlay from 'ol/Overlay';
-import View from 'ol/View';
-import Feature from 'ol/Feature';
-import Point from 'ol/geom/Point';
+import Map from 'ol/Map.js';
+import Overlay from 'ol/Overlay.js';
+import View from 'ol/View.js';
+import Feature from 'ol/Feature.js';
+import Point from 'ol/geom/Point.js';
+
 import Geocoder from 'ol-geocoder';
 import Popup from 'ol-popup';
 import axios from 'axios';
-import { fromLonLat, toLonLat } from 'ol/proj';
-import { Circle as CircleStyle, Stroke, Style, Fill, Text } from 'ol/style';
-import { OSM, Vector as VectorSource } from 'ol/source';
-import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
+
+import { fromLonLat, toLonLat } from 'ol/proj.js';
+import { Circle as CircleStyle, Stroke, Style, Fill, Text } from 'ol/style.js';
+import { OSM, Vector as VectorSource } from 'ol/source.js';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer.js';
 
 import { trilaterate, fromLonLat_epsg4978, toLonLat_epsg4978 } from './trilaterate.js';
 import SnailShellMatrix from './SnailShellMatrix.js';
@@ -25,10 +27,12 @@ var usersIndex = {};
 var usersIgnored = [];
 var ssm, flashInterval, currentMarker;
 
+
 // OSM base layer
 var layer = new TileLayer({
   source: new OSM(),
 });
+
 
 // map init
 var map = new Map({
@@ -39,6 +43,7 @@ var map = new Map({
     zoom: 2,
   }),
 });
+
 
 // marker styles
 var stylePositions = new Style({
@@ -53,6 +58,7 @@ var stylePositions = new Style({
     radius: 7,
   }),
 });
+
 
 let styleUsers = new Style({
   image: new CircleStyle({
@@ -73,18 +79,27 @@ let styleUsers = new Style({
   }),
 });
 
+
 // position markers
-var source = new VectorSource({ wrapX: false });
+var source = new VectorSource({
+  wrapX: false,
+});
+
 
 var vector = new VectorLayer({
   source: source,
   style: stylePositions,
 });
 
+
 map.addLayer(vector);
 
+
 // user markers
-var sourceUsers = new VectorSource({ wrapX: false });
+var sourceUsers = new VectorSource({
+  wrapX: false,
+});
+
 
 var vectorUsers = new VectorLayer({
   source: sourceUsers,
@@ -94,7 +109,9 @@ var vectorUsers = new VectorLayer({
   },
 });
 
+
 map.addLayer(vectorUsers);
+
 
 // geocoder
 var geocoder = new Geocoder('nominatim', {
@@ -107,10 +124,13 @@ var geocoder = new Geocoder('nominatim', {
   autoComplete: true,
 });
 
+
 map.addControl(geocoder);
+
 
 // flash animation
 source.on('addfeature', function (e) {
+
   currentMarker = e.feature;
 
   flashFeature(map, layer, currentMarker, flashDuration);
@@ -119,29 +139,41 @@ source.on('addfeature', function (e) {
     () => flashFeature(map, layer, currentMarker, flashDuration),
     2000
   );
+
 });
+
 
 // add position marker
 function addPositionMarker(c) {
+
   var geom = new Point(c);
   var feature = new Feature(geom);
+
   source.addFeature(feature);
+
 }
+
 
 // add user marker
 function addUserMarker(c, text) {
+
   var geom = new Point([
     c[0] + Math.floor(Math.random()) * 20,
     c[1] - Math.floor(Math.random()) * 20,
   ]);
 
   var feature = new Feature(geom);
+
   feature.set('name', text);
+
   sourceUsers.addFeature(feature);
+
 }
+
 
 // nearby search
 function searchNearby(searchCoordinates) {
+
   if (cancel) return;
 
   addPositionMarker(searchCoordinates);
@@ -151,6 +183,7 @@ function searchNearby(searchCoordinates) {
   axios
     .post('/getNearby', { lon: c[0], lat: c[1] })
     .then((res) => {
+
       clearInterval(flashInterval);
 
       let nextPoint = ssm.getNext()[1];
@@ -158,18 +191,25 @@ function searchNearby(searchCoordinates) {
       setTimeout(() => searchNearby(nextPoint), searchInterval);
 
       refreshNearby(searchCoordinates, res.data);
+
     })
     .catch((error) => {
+
       alert(error);
       console.error(error);
+
     });
+
 }
+
 
 // refresh user list
 function refreshNearby(coordinates, nearbyUsers) {
+
   let now = new Date().getTime();
 
   for (let i in nearbyUsers) {
+
     let d = {
       time: now,
       coordinates: coordinates,
@@ -177,6 +217,7 @@ function refreshNearby(coordinates, nearbyUsers) {
     };
 
     if (users[i] === undefined) {
+
       users[i] = {};
 
       users[i].relId = ++userIdIndex;
@@ -192,12 +233,15 @@ function refreshNearby(coordinates, nearbyUsers) {
 
       users[i].distances = [d];
       users[i].locations = [];
+
     } else {
+
       users[i].distances.push(d);
 
       if (usersIgnored.includes(users[i].relId)) continue;
 
       if (users[i].distances.length % 3 === 0) {
+
         let lastDistances = users[i].distances.slice(
           users[i].distances.length - 3
         );
@@ -217,6 +261,7 @@ function refreshNearby(coordinates, nearbyUsers) {
         let triP = trilaterate(p1, p2, p3, true);
 
         if (triP !== null) {
+
           let triLonLat = toLonLat_epsg4978(triP);
 
           let triC = fromLonLat([triLonLat.lon, triLonLat.lat]);
@@ -227,8 +272,13 @@ function refreshNearby(coordinates, nearbyUsers) {
           });
 
           addUserMarker(triC, users[i].relId.toString());
+
         }
+
       }
+
     }
+
   }
+
 }
